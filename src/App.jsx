2501a0f3e7f24398b49f469042942e9bc72da775
ABU TAHER <abutaher267@gmail.com>
2023-06-17 12:46:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
+
+export const Home = () => {
+  const login = () => {
+    const url = "http://localhost:4001/auth";
+    fetch(`${url}/login`, {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((d) => d.json())
+      .then((d) => {
+        console.log(d);
+      })
+      .catch(console.error);
+  };
+  const credentials = () => {
+    const url = "http://localhost:4001/auth";
+    fetch(`${url}/credentials`, {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((d) => d.json())
+      .then((d) => {
+        console.log(d);
+      })
+      .catch(console.error);
+  };
+  const logout = () => {
+    const url = "http://localhost:4001/auth";
+    fetch(`${url}/logout`, {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((d) => d.json())
+      .then((d) => {
+        console.log(d);
+      })
+      .catch(console.error);
+  };
+  return (
+    <>
+      <button onClick={login}>Login</button> |{" "}
+      <button onClick={credentials}>Credentils</button> |
+      <button onClick={logout}>logout</button>
+    </>
+  );
+};
